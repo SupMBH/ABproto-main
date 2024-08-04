3204d3650d52092c3eb12194b20a51c2d2b57973
supmbh <supmbh@gmail.com>
2024-08-04 16:20:48 +0200

    +lien fonctionnel nom redirige vers page user (encapsulation profileFirstName avec un Link qui redirige vers la page /user)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,58 +1,53 @@
 import React, { useEffect, useState } from 'react';
-
 import { Link, useNavigate } from 'react-router-dom';
-
-import logo from '../assets/argentBankLogo.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSignOut, faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import logo from '../assets/argentBankLogo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOut, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { logOut, selectCurrentToken } from '../_redux/authSlice';
 import { selectCurrentFirstName, unsetProfile } from '../_redux/profileSlice';
 
-
 const Header = () => {
-
-    const [loggedIn, setLoggedIn] = useState(false)
-    const [profileFirstName, setProfileFirstName] = useState('')
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [profileFirstName, setProfileFirstName] = useState('');
 
     // retieve user firstname and the token from the store
+    const firstName = useSelector(selectCurrentFirstName);
+    const token = useSelector(selectCurrentToken);
 
-    const firstName = useSelector(selectCurrentFirstName)
-    const token = useSelector(selectCurrentToken)
-
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (firstName) {
-            setProfileFirstName(firstName)
+            setProfileFirstName(firstName);
         }
         if (token) {
-            setLoggedIn(true)
+            setLoggedIn(true);
         } else {
-            setLoggedIn(false)
+            setLoggedIn(false);
         }
-    }, [firstName, token])
+    }, [firstName, token]);
 
     const handleLogOut = () => {
         // call the logOut function
-        dispatch(logOut())
+        dispatch(logOut());
         // clear the profile
-        dispatch(unsetProfile())
-        setProfileFirstName('')
-        setLoggedIn(false)
+        dispatch(unsetProfile());
+        setProfileFirstName('');
+        setLoggedIn(false);
         // send to the welcome page
-        navigate("/")
-    }
+        navigate('/');
+    };
 
     return (
         <>
             <nav className="main-nav">
                 <Link to="/">
                     <img
-                    className="main-nav-logo-image"
-                    src={logo}
-                    alt="Argent Bank Logo"
+                        className="main-nav-logo-image"
+                        src={logo}
+                        alt="Argent Bank Logo"
                     />
                     <h1 className="sr-only">Argent Bank</h1>
                 </Link>
@@ -65,12 +60,12 @@ const Header = () => {
                         </Link>
                     </div>
                 ) : (
-                    <div className='accountLog'>
-                        <div className='profileName'>  
+                    <div className="accountLog">
+                        <Link to="/user" className="profileName">
                             <FontAwesomeIcon icon={faUserCircle} className="icon-sign"></FontAwesomeIcon>
                             {profileFirstName}
-                        </div>
-                        <div className='logOut main-nav-item' onClick={handleLogOut}>
+                        </Link>
+                        <div className="logOut main-nav-item" onClick={handleLogOut}>
                             <FontAwesomeIcon icon={faSignOut} className="icon-sign"></FontAwesomeIcon>
                             Sign Out
                         </div>
